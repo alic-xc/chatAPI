3f3cf2e129a611d5fc79490fea85d4ab47effd54
alade lekan <aladelekan187@gmail.com>
2022-02-28 00:59:11 +0100

    Deploy: fix deployment

--- Procfile
+++ Procfile
@@ -1,6 +1,9 @@
 release: python manage.py migrate
 
 web: daphne -b 0.0.0.0 -p $PORT chatAPI.asgi:application
+worker: celery -A chatAPI worker -l INFO
+beat: celery -A chatAPI beat -l INFO
+chatworker: python manage.py runworker --settings=chatAPI.settings
 
 python manage.py collectstatic --noinput
 python manage.py runserver
--- chatAPI/asgi.py
+++ chatAPI/asgi.py
@@ -7,16 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
+import django
 from app import routing
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application, ProtocolTypeRouter, URLRouter
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatAPI.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_default_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
           routing.websocket_urlpatterns
